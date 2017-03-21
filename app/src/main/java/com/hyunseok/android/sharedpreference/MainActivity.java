cc2725bc0fd572e9243ab65e3409e1b10289a7ad
attentionrobber <lorch987@gmail.com>
2017-03-21 12:16:31 +0900

    getPreferences에서 getSharedPreferences로 변경

--- app/src/main/java/com/hyunseok/android/sharedpreference/MainActivity.java
+++ app/src/main/java/com/hyunseok/android/sharedpreference/MainActivity.java
@@ -1,5 +1,7 @@
 package com.hyunseok.android.sharedpreference;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +16,8 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String SHARED_FILE = "prop";
 
     EditText et_email;
     Switch switch_shuffle;
@@ -37,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         if("false".equals(propertyUtil.getProperty("firstOpen"))) {
             layoutHelp.setVisibility(View.GONE);
         }
+
+        loadSetting(); // 세팅된 값을 가져와 화면에 뿌린다.
     }
 
     public void closeHelp(View view) {
@@ -45,7 +51,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void saveSetting(View view) {
+        // 1. Preference 생성하기
+        SharedPreferences sharedPref = getSharedPreferences(SHARED_FILE, Context.MODE_PRIVATE); // SharedPreferences는 앱 단위로 저장이 된다.
+        //SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE); // Activity 단위로 저장이된다(xml파일로)
+        // 2. SharedPreference에 값을 입력하기 위해서는 에디터를 통해서만 가능
+        SharedPreferences.Editor editor = sharedPref.edit();
 
+        // editor.putInt("키", "값");
+        editor.putString("email", et_email.getText().toString());
+        editor.putBoolean("shuffle", switch_shuffle.isChecked());
+
+        // 3. 입력된 값을 반영한다.
+        editor.commit();
     }
 
+    public void loadSetting() {
+        SharedPreferences sharedPref = getSharedPreferences(SHARED_FILE, Context.MODE_PRIVATE);
+
+        String email = sharedPref.getString("email", null); // null이면 default값이 들어간다.
+        boolean shuffle = sharedPref.getBoolean("shuffle", false);
+
+        et_email.setText(email);
+        switch_shuffle.setChecked(shuffle);
+    }
 }
